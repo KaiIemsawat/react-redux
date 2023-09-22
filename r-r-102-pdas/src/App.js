75dd7bdfa916f2d6fd2077598f600300afd98944
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-22 08:14:14 -0400

    install bulma css. apply basic css card

--- r-r-102-pdas/src/App.js
+++ r-r-102-pdas/src/App.js
@@ -1,12 +1,43 @@
+import "bulma/css/bulma.css";
+
 import ProfileCard from "./ProfileCard";
+
+import AlexaImg from "./images/alexa.png";
+import CortanaImg from "./images/cortana.png";
+import SiriImg from "./images/siri.png";
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistantas</div>
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                image={AlexaImg}
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                image={CortanaImg}
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                image={SiriImg}
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- r-r-102-pdas/src/ProfileCard.js
+++ r-r-102-pdas/src/ProfileCard.js
@@ -1,8 +1,17 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
     return (
-        <div>
-            <div>Title : {title}</div>
-            <div>Handle : {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={title} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
         </div>
     );
 }
