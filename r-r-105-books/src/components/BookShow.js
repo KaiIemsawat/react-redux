ffde507446b90f8a0b86941163312e58dd7b3e04
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-24 15:38:58 -0400

    added random image to each post

--- r-r-105-books/src/components/BookShow.js
+++ r-r-105-books/src/components/BookShow.js
@@ -24,6 +24,10 @@ function BookShow({ book, onDelete, onEditBook }) {
 
     return (
         <div className="book-show">
+            <img
+                src={`https://picsum.photos/seed/${book.id}/300/200`}
+                alt="Book"
+            />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={clickToEditHandler}>
