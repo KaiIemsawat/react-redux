b8637782d2fbebd162e71f3e1b74ff79f335199c
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-25 08:21:00 -0400

    added edit feature that edit data in database using axios

--- r-r-105-books/src/App.js
+++ r-r-105-books/src/App.js
@@ -1,31 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (editingBookId, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+
+        setBooks(response.data);
+    };
+
+    // Note below
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
+        console.log(response);
+
         const updatedBooks = books.map((thisBook) => {
-            if (thisBook.id === editingBookId) {
-                return { ...thisBook, title: newTitle };
+            if (thisBook.id === id) {
+                return { ...thisBook, ...response.data };
             }
             return thisBook;
         });
+
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((eaBook) => {
             return eaBook.id !== id;
         });
+
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
-        let id = Math.floor(Math.random() * 99999); // temporaly use since in reality, the id should be generate in the backend
+    const createBook = async (title) => {
+        const response = await axios.post("http://localhost:3001/books", {
+            title,
+        });
 
-        const updatedBooks = [...books, { id, title }];
+        const updatedBooks = [...books, response.data];
+
         setBooks(updatedBooks);
     };
 
@@ -42,3 +66,33 @@ function App() {
     );
 }
 export default App;
+
+/* 
+    -- have an empty aray as the second argument
+    --  ** Call the arrow function 'immediately after the first render'
+    --  ** and not ever call again
+
+    useEffect(() => {
+      sumeFunction();
+    }, [])
+
+    -------------------------------------------------------------
+
+    -- no array, no second argument
+    --  ** Call the arrow function 'immediately after the first render'
+    --  ** Call after 'every rerender'
+    
+    useEffect(() => {
+      sumeFunction();
+    })
+
+        -------------------------------------------------------------
+
+    -- no array, no second argument
+    --  ** Call the arrow function 'immediately after the first render'
+    --  ** Call again if 'counter' variable 'changed;
+
+    useEffect(() => {
+      sumeFunction();
+    }, [counter])
+    */
