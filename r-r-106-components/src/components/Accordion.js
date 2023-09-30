af78e4386eebf00406e15b2c7b54f8c7e5e250c8
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-29 22:25:47 -0400

    base work for custom dropdown panel

--- r-r-106-components/src/components/Accordion.js
+++ r-r-106-components/src/components/Accordion.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function Accordion({ items }) {
-    const renderItems = items.map((item) => {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const clickHandler = (i) => {
+        setExpandedIndex(i);
+    };
+
+    const renderItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const icon = (
+            <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+        );
+
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
-                <div>{item.content}</div>
+                <div onClick={() => clickHandler(index)}>
+                    {icon}
+                    {item.label}
+                </div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
