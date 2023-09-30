22bb2fd62d5ef3a65bdf1dedb87301df41b125ae
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-30 12:04:09 -0400

    finished working on hide/show content when click

--- r-r-106-components/src/components/Accordion.js
+++ r-r-106-components/src/components/Accordion.js
@@ -3,31 +3,42 @@ import { useState } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
     const clickHandler = (i) => {
-        setExpandedIndex(i);
+        setExpandedIndex((currentExpendedIndex) => {
+            if (currentExpendedIndex === i) {
+                return -1;
+            } else return i;
+        });
     };
 
     const renderItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         const icon = (
-            <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+            <span className="text-xl">
+                {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
         );
 
         return (
             <div key={item.id}>
-                <div onClick={() => clickHandler(index)}>
-                    {icon}
+                <div
+                    className="flex p-3 justify-between bg-gray-50 border-b items-center cursor-pointer"
+                    onClick={() => clickHandler(index)}
+                >
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && (
+                    <div className="border-b p-5">{item.content}</div>
+                )}
             </div>
         );
     });
 
-    return <div>{renderItems}</div>;
+    return <div className="border-x border-t rounded">{renderItems}</div>;
 }
 
 export default Accordion;
