f23fd44c921428221af7126cf8eecc4466570bc4
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-30 22:47:24 -0400

    added some styling in separated component

--- r-r-106-components/src/components/Dropdown.js
+++ r-r-106-components/src/components/Dropdown.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -21,6 +23,7 @@ function Dropdown({ options, value, onChange }) {
     const renderedOptions = options.map((eachOption) => {
         return (
             <div
+                className="hover:bg-sky-100 rounded cursor-pointer p-1"
                 onClick={() => optionClickHandler(eachOption)}
                 key={eachOption.value}
             >
@@ -30,9 +33,17 @@ function Dropdown({ options, value, onChange }) {
     });
 
     return (
-        <div>
-            <div onClick={clickHandler}>{value?.label || "Select..."}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className="w-48 relative">
+            <Panel
+                className="flex justify-between items-center cursor-pointer"
+                onClick={clickHandler}
+            >
+                {value?.label || "Select..."}
+                <GoChevronDown />
+            </Panel>
+            {isOpen && (
+                <Panel className="absolute top-full">{renderedOptions}</Panel>
+            )}
         </div>
     );
 }
