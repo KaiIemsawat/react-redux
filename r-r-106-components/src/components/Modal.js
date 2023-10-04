7369a76fedc704a04876d2dcf90c259274a963eb
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-03 20:15:07 -0400

    fixed css issue (absolute to fixed)

--- r-r-106-components/src/components/Modal.js
+++ r-r-106-components/src/components/Modal.js
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function Modal({ onClose, children, actionBar }) {
+    useEffect(() => {
+        document.body.classList.add("overflow-hidden");
+        // With this (above), the content will not move once the 'modal' appears
+        // But once the 'modal' is removed, the content still remain frozen
+        // The return is need to remove the frozen condition
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, []);
+
     return ReactDOM.createPortal(
         <div>
             <div
                 onClick={onClose}
-                className="absolute inset-0 bg-gray-300 opacity-80"
+                className="fixed inset-0 bg-gray-300 opacity-80"
             ></div>
-            <div className="absolute inset-40 p-10 bg-white">
-                {children}
-                {actionBar}
+            <div className="fixed inset-40 p-10 bg-white">
+                <div className="flex flex-col justify-between h-full">
+                    {children}
+                    <div className="flex justify-end">{actionBar}</div>
+                </div>
             </div>
         </div>,
         document.querySelector(".modal-container")
--- r-r-106-components/src/pages/ModalPage.js
+++ r-r-106-components/src/pages/ModalPage.js
@@ -27,12 +27,46 @@ function ModalPage() {
         </Modal>
     );
 
+    const dummyParagraph = (
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac
+            augue nisl. Etiam congue auctor mattis. Integer odio magna, commodo
+            vitae eros eu, blandit commodo ligula. Phasellus eget massa id metus
+            cursus elementum. Curabitur iaculis a nibh sit amet viverra. Nunc
+            vitae augue ac elit pellentesque mollis efficitur vitae nulla. Orci
+            varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus. Mauris sem augue, sollicitudin et metus tempor,
+            aliquet vehicula tellus. Suspendisse sollicitudin tristique quam
+            vitae tempor. Sed id velit sed felis sagittis mattis quis vestibulum
+            sem. Vivamus at imperdiet augue, sit amet tincidunt purus. Cras
+            posuere leo ac vehicula posuere. Curabitur vulputate, enim sit amet
+            lacinia ullamcorper, arcu ligula feugiat tortor, non faucibus ex
+            risus id nibh. Nam nec pretium sapien. Etiam vestibulum, nisi in
+            congue consequat, mi urna convallis orci, in fermentum ante metus at
+            enim.
+        </p>
+    );
+
     return (
         <div>
             <Button onClick={clickHandler} primary>
                 Open Modal
             </Button>
             {showModal && modal}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
+            {dummyParagraph}
         </div>
     );
 }
