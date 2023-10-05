3f88d86d6f31cde4de2daf4834d6b4254a5e561f
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-05 19:22:58 -0400

    create custom hook

--- r-r-106-components/src/pages/CounterPage.js
+++ r-r-106-components/src/pages/CounterPage.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
 
-function CounterPage({ initialCount }) {
+/* create custom hook */
+function useSomething(initialCount) {
+    // <--- take initialCount but not as agrument not props object
+
     const [count, setCount] = useState(initialCount);
 
     useEffect(() => {
@@ -11,6 +14,28 @@ function CounterPage({ initialCount }) {
     const clickHandler = () => {
         setCount(count + 1);
     };
+
+    return {
+        count,
+        clickHandler,
+    };
+}
+
+function CounterPage({ initialCount }) {
+    /* Move these blocks of code to the custom hook */
+    // const [count, setCount] = useState(initialCount);
+
+    // useEffect(() => {
+    //     console.log(count);
+    // }, [count]);
+
+    // const clickHandler = () => {
+    //     setCount(count + 1);
+    // };
+
+    // destruct object from the custom hook
+    const { count, clickHandler } = useSomething(initialCount);
+
     return (
         <div>
             <h1>Count is {count}</h1>
