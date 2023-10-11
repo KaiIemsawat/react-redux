ad0c6dae9b0247d69a5c74df8f787f00704ddc0c
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-11 18:11:48 -0400

    added input field for cost. filter input field to not allow 'e' in number type input field

--- r-r-108-cars/src/components/CarForm.js
+++ r-r-108-cars/src/components/CarForm.js
@@ -1,17 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName } from "../store";
+import { changeName, changeCost } from "../store";
 
 function CarForm() {
     const dispatch = useDispatch();
 
-    const name = useSelector((state) => {
-        return state.form.name;
+    const { name, cost } = useSelector((state) => {
+        return {
+            name: state.form.name,
+            cost: state.form.cost,
+        };
     });
 
     const nameChangeHandler = (event) => {
         dispatch(changeName(event.target.value));
         // changeName is a reducer function imported from '../store' which also imported from 'formSlice.js'
         // And it expect a payload // SEE in fromSlice.js -> changeName(state, action) {state.name = action.payload},
+    };
+
+    const costChangeHandler = (event) => {
+        const carCost = parseInt(event.target.value) || 0;
+
+        dispatch(changeCost(carCost));
     };
 
     return (
@@ -26,6 +35,19 @@ function CarForm() {
                             className="input is-expanded"
                             value={name}
                             onChange={nameChangeHandler}
+                        />
+                    </div>
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input
+                            className="input is-expanded"
+                            value={cost || ""} // use '|| ""' to eliminate '0' that remain in the field
+                            onChange={costChangeHandler}
+                            type="number"
+                            onKeyDown={(evt) =>
+                                ["e", "E", "+", "-"].includes(evt.key) &&
+                                evt.preventDefault()
+                            }
                         />
                     </div>
                 </div>
