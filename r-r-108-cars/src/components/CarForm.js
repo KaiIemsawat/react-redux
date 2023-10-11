5f3ca84883c9b5b3154670162862caa0f16a4ec8
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-11 10:11:33 -0400

    apply state to CarForm

--- r-r-108-cars/src/components/CarForm.js
+++ r-r-108-cars/src/components/CarForm.js
@@ -1,5 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 function CarForm() {
-    return <div>CarForm</div>;
+    const dispatch = useDispatch();
+
+    const name = useSelector((state) => {
+        return state.form.name;
+    });
+
+    const nameChangeHandler = (event) => {
+        dispatch(changeName(event.target.value));
+        // changeName is a reducer function imported from '../store' which also imported from 'formSlice.js'
+        // And it expect a payload // SEE in fromSlice.js -> changeName(state, action) {state.name = action.payload},
+    };
+
+    return (
+        <div className="car-form panel">
+            <h4 className="subtitle is-3">Add Car</h4>
+            <form>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input
+                            type="text"
+                            className="input is-expanded"
+                            value={name}
+                            onChange={nameChangeHandler}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default CarForm;
