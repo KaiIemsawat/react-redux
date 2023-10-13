f0c593d174ecf75a9838fb5719d12a69040c30b5
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-13 14:43:56 -0400

    loading state (first approach)

--- r-r-109-media/src/components/UsersList.js
+++ r-r-109-media/src/components/UsersList.js
@@ -1,29 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 
 function UsersList() {
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [loadingUsersError, setLoadingUsersError] = useState(null);
     const dispatch = useDispatch();
-    const { isLoading, data, error } = useSelector((state) => {
+    const { data } = useSelector((state) => {
         //                                    this 'state' <-- {data: [],isLoading: false,error: null} from usersSlice.js
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers())
+            .unwrap()
+            .catch((error) => setLoadingUsersError(error))
+            .finally(() => setIsLoadingUsers(false));
     }, [dispatch]);
 
     const userAddHandler = () => {
         dispatch(addUser());
     };
 
-    if (isLoading) {
+    if (isLoadingUsers) {
         return <Skeleton times={5} className="h-10 w-full" />;
     }
 
-    if (error) {
+    if (loadingUsersError) {
         return <div>Error fetching data....</div>;
     }
 
--- r-r-109-media/src/store/thunks/fetchUser.js
+++ r-r-109-media/src/store/thunks/fetchUser.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
     const response = await axios.get("http://localhost:3005/users");
 
-    await pause(2000); // DEV ONLY !!
+    await pause(1000); // DEV ONLY !!
 
     return response.data;
 });
