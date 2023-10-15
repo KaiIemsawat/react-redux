8c0f32634d1a4fa633769db47092c06c02bf1af7
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-10-15 19:50:05 -0400

    refactor for expandable panel

--- r-r-109-media/src/components/UserListItem.js
+++ r-r-109-media/src/components/UserListItem.js
@@ -2,28 +2,22 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 export default function UserListItem({ user }) {
     const [doRemoveUser, isLoading, error] = useThunk(removeUser);
     const clickHandler = () => {
         doRemoveUser(user);
     };
-
-    return (
-        <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className="flex flex-row items-center justify-between">
-                    <Button
-                        className="mr-3"
-                        loading={isLoading}
-                        onClick={clickHandler}
-                    >
-                        <GoTrash />
-                    </Button>
-                    {error && <div>Error deleting user....</div>}
-                    {user.name}
-                </div>
-            </div>
-        </div>
+    const header = (
+        <>
+            <Button className="mr-3" loading={isLoading} onClick={clickHandler}>
+                <GoTrash />
+            </Button>
+            {error && <div>Error deleting user....</div>}
+            {user.name}
+        </>
     );
+
+    return <ExpandablePanel header={header}>CONTENT..!!</ExpandablePanel>;
 }
